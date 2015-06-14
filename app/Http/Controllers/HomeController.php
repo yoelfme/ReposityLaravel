ef3dc16983319014d0a9f2b2c85e42fc9dfb11a8
Yoel Monzon <yoelfme@hotmail.com>
2015-06-13 23:49:37 -0600

    [update] files and remove unnecessary

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,8 @@
-<?php namespace App\Http\Controllers;
+<?php 
+namespace App\Http\Controllers;
 
-class HomeController extends Controller {
+class HomeController extends Controller 
+{
 
 	/*
 	|--------------------------------------------------------------------------
